92ef97fcf3dbb7230fd6d77600a43d0d65f9a43c
Vadim Manushin <vadimmanushin@yandex.ru>
2022-12-11 21:12:31 +0300

    fix: correction of styles of ships and adding stypes for ship-field

--- src/components/ship/ship.ts
+++ src/components/ship/ship.ts
@@ -1,5 +1,5 @@
 class Ship {
-  private isVertical: boolean;
+  private isVertical: boolean | undefined;
   private shipDiv: HTMLDivElement;
   private size: number;
 
@@ -10,15 +10,19 @@ class Ship {
   }
 
   private detectPositionAndSizeOfShip(shipDiv: HTMLDivElement) {
-    if (shipDiv.classList.contains('ship_is-vertical')) {
-      this.isVertical = true;
-    }
-    this.isVertical = false;
     this.shipDiv.classList.forEach((c) => {
       if (c.includes('ship-')) {
         this.size = Number(c.split('-')[1]);
       }
     });
+    if (this.size === 1) {
+      this.isVertical = undefined;
+    } else {
+      if (shipDiv.classList.contains('ship_is-vertical')) {
+        this.isVertical = true;
+      }
+      this.isVertical = false;
+    }
   }
 
   private bindEvents() {
